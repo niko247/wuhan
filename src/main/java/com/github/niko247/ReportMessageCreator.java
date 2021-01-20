38c6a09a30e21fc75425b1e46a9e24eef3b0cf13
John <mail4tck@gmail.com>
2021-01-20 15:32:06 +0100

    Fixed polish locale

--- src/main/java/com/github/niko247/ReportMessageCreator.java
+++ src/main/java/com/github/niko247/ReportMessageCreator.java
@@ -2,6 +2,7 @@ package com.github.niko247;
 
 import lombok.extern.log4j.Log4j2;
 
+import java.util.Locale;
 import java.util.Optional;
 import java.util.function.ToIntFunction;
 import java.util.stream.Stream;
@@ -38,7 +39,7 @@ public class ReportMessageCreator {
 
         var difference = currentCounterResult - oldCounterResult;
         if (difference != 0) {
-            return Optional.of(String.format("%s: %,d (%+,d).", messagePrefix, currentCounterResult, difference));
+            return Optional.of(String.format(new Locale("pl", "PL"), "%s: %,d (%+,d).", messagePrefix, currentCounterResult, difference));
         }
         return Optional.empty();
     }

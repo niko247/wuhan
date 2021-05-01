fd9b84650e5e91eabe1bdeeebf34205ccb96f7e7
John <mail4tck@gmail.com>
2021-05-01 17:25:01 +0200

    Fix nullpointer

--- src/main/java/com/github/niko247/CoronaResultsFetcher.java
+++ src/main/java/com/github/niko247/CoronaResultsFetcher.java
@@ -9,7 +9,7 @@ import org.jsoup.nodes.Document;
 import java.io.IOException;
 
 public class CoronaResultsFetcher {
-    private static final String URL = "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_corona_widok2/FeatureServer/0/query?f=json&where=Data%20BETWEEN%20(CURRENT_TIMESTAMP%20-%20INTERVAL%20%2724%27%20HOUR)%20AND%20CURRENT_TIMESTAMP&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard";
+    private static final String URL = "https://services-eu1.arcgis.com/zk7YlClTgerl62BY/arcgis/rest/services/global_corona_actual_widok3/FeatureServer/0/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&resultOffset=0&resultRecordCount=1&resultType=standard&cacheHint=true";
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36";
 
     public SummaryResults fetchCases() throws IOException {
